9f69404f6945f53464e4a339f4995b9502aefeb6
hibaHAJAJI <wiamhiba08@gmail.com>
2026-08-20 23:16:59 +0100

    Initialiser Spring Boot

---
+++ src/test/java/ServiCasa/ApplicationTests.java
@@ -0,0 +1,13 @@
+package ServiCasa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
