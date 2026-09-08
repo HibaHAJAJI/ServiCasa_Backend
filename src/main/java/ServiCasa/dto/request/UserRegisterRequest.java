f2d921b1ac91b0334ace1a4108bbb37b2995c00c
hibaHAJAJI <wiamhiba08@gmail.com>
2026-09-08 12:08:35 +0100

    feat: add user validation

--- src/main/java/ServiCasa/dto/request/UserRegisterRequest.java
+++ src/main/java/ServiCasa/dto/request/UserRegisterRequest.java
@@ -1,21 +1,35 @@
 package ServiCasa.dto.request;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 import ServiCasa.enums.Role;
 
 @Data
 public class UserRegisterRequest {
 
+    @NotBlank(message = "Le nom est obligatoire")
     private String nom;
 
+    @NotBlank(message = "Le prénom est obligatoire")
     private String prenom;
 
+    @NotBlank(message = "Le téléphone est obligatoire")
+    @Size(min = 6, max = 16, message = "Le numéro doit contenir entre 4 et 16 chiffres !")
+    @Pattern(regexp = "^[0-9]+$" ,message = "Le champs entrer uniquement les chiffres")
     private String telephone;
 
+    @NotBlank(message = "La ville est obligatoire")
     private String ville;
 
+    @NotBlank(message = "L'email est obligatoire")
+    @Email(message = "L'email doit être valide")
     private String email;
 
+    @NotBlank(message = "Le mot de passe est obligatoire")
+    @Size(min = 6, message = "Le mot de passe doit contenir au moins 8 caractères")
     private String password;
 
     private Role role;
