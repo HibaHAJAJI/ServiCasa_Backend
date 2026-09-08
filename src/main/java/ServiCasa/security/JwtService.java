85cbb7fe3938b5452ea580fdf22eaaaf4c09f088
hibaHAJAJI <wiamhiba08@gmail.com>
2026-09-08 10:04:33 +0100

    feat: add jwt service

--- src/main/java/ServiCasa/security/JwtService.java
+++ src/main/java/ServiCasa/security/JwtService.java
@@ -1,4 +1,42 @@
 package ServiCasa.security;
 
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
+
+import javax.crypto.SecretKey;
+import java.nio.charset.StandardCharsets;
+import java.util.Date;
+import java.util.Map;
+
+
+@Service
 public class JwtService {
+
+    @Value("${jwt.secret}")
+    private  String secretKey;
+
+    @Value("${jwt.expiration}")
+    private  long jwtExpiration;
+
+    public String generateToken(Map<String, Object> extraClaims, UserDetails userDetails) {
+
+        return Jwts
+                .builder()
+                .claims(extraClaims)
+                .subject(userDetails.getUsername())
+                .issuedAt(new Date(System.currentTimeMillis()))
+                .expiration(new Date(System.currentTimeMillis() + jwtExpiration))
+                .signWith(getSignInKey())
+                .compact();
+    }
+
+    private SecretKey getSignInKey() {
+
+        return Keys.hmacShaKeyFor(secretKey.getBytes(StandardCharsets.UTF_8));
+    }
+
 }
