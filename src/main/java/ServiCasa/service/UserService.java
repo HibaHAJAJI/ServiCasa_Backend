02cdd8eaf43c835788d2032798a2daff2997e659
hibaHAJAJI <wiamhiba08@gmail.com>
2026-09-08 09:50:57 +0100

    feat: add user details service

--- src/main/java/ServiCasa/service/UserService.java
+++ src/main/java/ServiCasa/service/UserService.java
@@ -1,5 +1,27 @@
 package ServiCasa.service;
 
 
-public class UserService {
+import ServiCasa.entity.User;
+import ServiCasa.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+
+@Service
+@RequiredArgsConstructor
+public class UserService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found with email: " + email));
+
+        return user;
+
+    }
 }
