52e7ceaa6461c4fa20d555ba9968dde2888825af
hibaHAJAJI <wiamhiba08@gmail.com>
2026-09-08 12:29:15 +0100

    feat: add availability validation

--- src/main/java/ServiCasa/dto/request/DisponibiliteRequestDTO.java
+++ src/main/java/ServiCasa/dto/request/DisponibiliteRequestDTO.java
@@ -1,5 +1,6 @@
 package ServiCasa.dto.request;
 
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 import java.time.DayOfWeek;
@@ -9,14 +10,19 @@ import java.time.LocalTime;
 @Data
 public class DisponibiliteRequestDTO {
 
+    @NotNull(message = "Le jour est obligatoire")
     private DayOfWeek jour;
 
+    @NotNull(message = "L'heure de début est obligatoire")
     private LocalTime heureDebut;
 
+    @NotNull(message = "L'heure de fin est obligatoire")
     private LocalTime heureFin;
 
-    private Boolean disponible;
+    @NotNull(message = "L'état de disponibilité est obligatoire")
+    private Boolean disponibilite;
 
+    @NotNull(message = "L'ID de l'artisan est obligatoire")
     private Long artisanId;
 
 }
